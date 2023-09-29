0818427f409d24ebc3b68a9434b40e46c174c8ae
Tayo Jubril <jubriltayo@gmail.com>
2023-09-30 00:33:54 +0100

    allSettled

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,14 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, filename) {
+  const userPromise = signUpUser(firstName, lastName);
+  const uploadPromise = uploadPhoto(filename);
+
+  return Promise.allSettled([userPromise, uploadPromise]).then((results) => {
+    results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.console.error(),
+    }));
+  });
+}
