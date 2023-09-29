786ffbeffc3bde18d4c9c4ad74788503800a19a7
Tayo Jubril <jubriltayo@gmail.com>
2023-09-30 00:57:34 +0100

    allSettled

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,11 +1,11 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
 
-export default function handleProfileSignup(firstName, lastName, filename) {
-  const userPromise = signUpUser(firstName, lastName);
-  const uploadPromise = uploadPhoto(filename);
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const createPromise = signUpUser(firstName, lastName);
+  const uploadPromise = uploadPhoto(fileName);
 
-  return Promise.allSettled([userPromise, uploadPromise]).then((results) => {
+  return Promise.allSettled([createPromise, uploadPromise]).then((results) => {
     return results.map((result) => ({
       status: result.status,
       value: result.status === 'fulfilled' ? result.value : result.error,
